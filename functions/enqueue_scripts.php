f4426d9fb543d47bb46ee42d665fdae23e9d7397
Philip Newborough <corenominal@corenominal.org>
2016-03-22 20:31:01 +0000

    Some CSS stuff

--- functions/enqueue_scripts.php
+++ functions/enqueue_scripts.php
@@ -7,6 +7,7 @@ function llug_enqueue_scripts()
     /**
      * Default Styles
      */
+    wp_enqueue_style( 'reset', get_template_directory_uri() . '/css/vendor/reset.css', false );
     wp_enqueue_style( 'fontawesome', get_template_directory_uri() . '/css/vendor/font-awesome-4.5.0/css/font-awesome.min.css', false );
     wp_enqueue_style( 'grid12', get_template_directory_uri() . '/css/vendor/grid12.css', false );
     wp_enqueue_style( 'llug_css', get_template_directory_uri() . '/css/llug.css', false );
@@ -17,5 +18,14 @@ function llug_enqueue_scripts()
     wp_enqueue_script( 'jquery_js', get_template_directory_uri() . '/js/vendor/jquery-1.11.3.min.js', array(), false, true );
     wp_enqueue_script( 'jquery_plugins', get_template_directory_uri() . '/js/vendor/jquery.plugins.combined.js', array(), false, true );
     wp_enqueue_script( 'llug_js', get_template_directory_uri() . '/js/llug.js', array(), false, true );
+
+    /**
+     * Conditionals
+     */
+    if ( is_home() || is_front_page() )
+    {
+        wp_enqueue_style( 'llug_homepage', get_template_directory_uri() . '/css/homepage.css', false );
+    }
+
 }
 add_action( 'wp_enqueue_scripts', 'llug_enqueue_scripts' );
--- page-homepage.php
+++ page-homepage.php
@@ -1,109 +1,120 @@
 <?php
 get_header();
 ?>
-<div class="container">
-
-<div class="row">
-	<div class="col-md-4">
-		<?php
-		// LOGO
-		?>
-	</div>
-
-	<div class="col-md-7 col-md-offset-1">
-		<?php
-		// About Lincoln LUG
-		iewp_content_snippet( 13 );
-		?>
-	</div>
-</div>
-
-
-<?php
-// Show latest meeting
-$args = array (
-	'post_type'              => array( 'lug_meeting' ),
-	'post_status'            => array( 'published' ),
-	'posts_per_page'		 => 1, 
-);
-$query = new WP_Query( $args );
-if ( $query->have_posts() ) {
-	while ( $query->have_posts() ) {
-		$query->the_post();
-		?>
+<div class="container-dark">
+	<div class="container">
 		<div class="row">
-
-			<div class="col-md-6">
-				<h2><i class="fa fa-calendar"></i> Next Meeting</h2>
+			<div class="col-md-4">
 				<?php
-				// Post meta
-				$meta = iewp_lug_meeting_get_post_meta( $post->ID );
-				echo '<h2>' . date( 'l jS F G:i', $meta['startdate_timestamp'] ) . '</h2>';
-				the_content();
-				echo '<p><strong>Venue:</strong><br>';
-				if( $meta['venue_website'] != '' )
-				{
-					echo '<a href="' . $meta['venue_website'] . '" target="_blank">' . $meta['venue_name'] . '</a><br>';
-				}
-				else
-				{
-					echo $meta['venue_name'] . '<br>';
-				}
-				echo $meta['venue_address_street'] . ', ' . $meta['venue_address_city'] . '<br>';
-				echo $meta['venue_address_postcode'] . '</p>';
-
-				// Structured data
-				echo iewp_lug_meeting_structured_data( $post->ID );
+				// LOGO
 				?>
 			</div>
 
-			<div class="col-md-5 col-md-offset-1">
-				<h2><i class="fa fa-map"></i> Location</h2>
-				<a href="https://maps.google.com/maps?q=<?php echo $meta['venue_address_postcode'] ?>" target="_blank">
-				<img src="https://maps.google.com/maps/api/staticmap?center=<?php echo $meta['venue_address_postcode'] ?>&zoom=12&size=600x300&maptype=roadmap&markers=color:ORANGE|label:A|<?php echo $meta['venue_address_postcode'] ?>&scale=2&sensor=false " alt="Lincoln LUG meeting location">
-				</a>
+			<div class="col-md-7 col-md-offset-1">
+				<?php
+				// About Lincoln LUG
+				iewp_content_snippet( 13 );
+				?>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-light">
+	<div class="container">
+		<?php
+		// Show latest meeting
+		$args = array (
+			'post_type'              => array( 'lug_meeting' ),
+			'post_status'            => array( 'published' ),
+			'posts_per_page'		 => 1, 
+		);
+		$query = new WP_Query( $args );
+		if ( $query->have_posts() ) {
+			while ( $query->have_posts() ) {
+				$query->the_post();
+				?>
+				<div class="row">
+
+					<div class="col-md-6">
+						<h2><i class="fa fa-calendar"></i> Next Meeting</h2>
+						<?php
+						// Post meta
+						$meta = iewp_lug_meeting_get_post_meta( $post->ID );
+						echo '<h2>' . date( 'l jS F G:i', $meta['startdate_timestamp'] ) . '</h2>';
+						the_content();
+						echo '<p><strong>Venue:</strong><br>';
+						if( $meta['venue_website'] != '' )
+						{
+							echo '<a href="' . $meta['venue_website'] . '" target="_blank">' . $meta['venue_name'] . '</a><br>';
+						}
+						else
+						{
+							echo $meta['venue_name'] . '<br>';
+						}
+						echo $meta['venue_address_street'] . ', ' . $meta['venue_address_city'] . '<br>';
+						echo $meta['venue_address_postcode'] . '</p>';
+
+						// Structured data
+						echo iewp_lug_meeting_structured_data( $post->ID );
+						?>
+					</div>
+
+					<div class="col-md-5 col-md-offset-1">
+						<h2><i class="fa fa-map"></i> Location</h2>
+						<a href="https://maps.google.com/maps?q=<?php echo $meta['venue_address_postcode'] ?>" target="_blank">
+						<img src="https://maps.google.com/maps/api/staticmap?center=<?php echo $meta['venue_address_postcode'] ?>&zoom=12&size=600x300&maptype=roadmap&markers=color:ORANGE|label:A|<?php echo $meta['venue_address_postcode'] ?>&scale=2&sensor=false " alt="Lincoln LUG meeting location">
+						</a>
+					</div>
+
+				</div>
+				<?php
+			}
+		}
+		// Reset the loop
+		wp_reset_postdata();
+		?>
+	</div>
+</div>
+
+<div class="container-dark">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<?php
+				// IRC info
+				iewp_content_snippet( 14 );
+				?>
 			</div>
 
+			<div class="col-md-7 col-md-offset-1">
+				<?php
+				// FAQs
+				iewp_content_snippet( 16 );
+				?>
+			</div>
 		</div>
-		<?php
-	}
-}
-// Reset the loop
-wp_reset_postdata();
-?>
-
-<div class="row">
-	<div class="col-md-4">
-		<?php
-		// IRC info
-		iewp_content_snippet( 14 );
-		?>
-	</div>
-
-	<div class="col-md-7 col-md-offset-1">
-		<?php
-		// FAQs
-		iewp_content_snippet( 16 );
-		?>
 	</div>
 </div>
 
-<div class="row">
-	<div class="col-md-6">
-		<?php
-		// Social code
-		iewp_content_snippet( 18 );
-		?>
-	</div>
+<div class="container-light">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<?php
+				// Social code
+				iewp_content_snippet( 18 );
+				?>
+			</div>
 
-	<div class="col-md-6">
-		<?php
-		// Copyleft
-		iewp_content_snippet( 19 );
-		?>
+			<div class="col-md-6">
+				<?php
+				// Copyleft
+				iewp_content_snippet( 19 );
+				?>
+			</div>
+		</div>
 	</div>
-</div>
-
 </div>
 <?php
 get_footer();
