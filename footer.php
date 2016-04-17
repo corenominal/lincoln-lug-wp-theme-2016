7c44401a6f99388d16ab54806720c7152a725a44
Philip Newborough <corenominal@corenominal.org>
2016-04-17 10:56:12 +0100

    Adjust footer for single pages

--- footer.php
+++ footer.php
@@ -1,7 +1,11 @@
 <footer class="container-dark">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-12">
+			<?php if( is_single() ): ?>
+				<div class="col-md-8 col-md-offset-4">
+			<?php else: ?>
+				<div class="col-md-12">
+			<?php endif; ?>
 				<?php
 				// Copyleft
 				iewp_content_snippet( 19 );
